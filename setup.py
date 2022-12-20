ee2b52e6c34e2d7809b55e3ae3b723a599566ca3
DelinteNicolas <nicolas.delinte@uclouvain.be>
2022-12-20 15:52:10 +0100

    added packaging files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+import binama
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "docs/README.md").read_text()
+
+setup(
+    name='pilab-binama',
+    version=binama.__version__,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/PiLAB-Medical-Imaging/BinaryMasks',
+    author='PiLAB',
+    author_email='nicolas.delinte@uclouvain.be',
+    license='GNU General Public License v3.0',
+    packages=['binama'],
+    install_requires=['numpy',
+                      'scikit-image',
+                      ],
+
+    classifiers=['Natural Language :: English',
+                 'Programming Language :: Python'],
+)
